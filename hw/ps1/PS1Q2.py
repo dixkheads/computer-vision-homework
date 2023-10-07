55c0f9ab70df45c9d5f69f7a5962d3cc0edfcffb
dixkheads <gyrocopterfaith@hotmail.com>
2023-10-07 18:11:28 -0400

    hw1

--- hw/ps1/PS1Q2.py
+++ hw/ps1/PS1Q2.py
@@ -1,63 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
 
 class Prob2():
     def __init__(self):
         """Load inputAPS1Q2.npy here as a class variable A."""
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-        
+        self.A = np.load('inputAPS1Q2.npy')
+        pass
+
     def prob_2_1(self):
         """Do plotting of intensities of A in decreasing value."""
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+        sorted_intensities = np.sort(self.A.flatten())[::-1]
+        colors = cm.Greys_r(sorted_intensities)
+        plot_height = 0.01
+
+        plt.figure(figsize=(10, 1))
+        plt.bar(range(len(sorted_intensities)), np.ones(len(sorted_intensities)),
+                color=colors, width=1.0)
+        plt.gca().axes.get_yaxis().set_visible(False)
+        plt.xlim(0, 10000)
+        plt.ylim(0, 1)
+        plt.title('2.1 Intensity Plot')
+        plt.show()
+        pass
+
     def prob_2_2(self):
         """Display histogram of A's intensities with 20 bins here."""
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+        plt.hist(self.A.flatten(), bins=20, edgecolor='black')
+        plt.xlabel('Intensity Range')
+        plt.ylabel('Frequency')
+        plt.title('2.2 Intensity Histogram')
+        plt.show()
+        pass
+
     def prob_2_3(self):
         """
         Create a new matrix X that consists of the bottom left quadrant of A here.
         Returns:
             X: bottom left quadrant of A which is of size 50 x 50
         """
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
-        return X 
-    
+        X = self.A[self.A.shape[0] // 2:, :self.A.shape[1] // 2]
+        # self.A = X
+        # self.prob_2_2()
+        pass
+
+        return X
+
     def prob_2_4(self):
         """Create a new matrix Y, which is the same as A, but with A’s mean intensity value subtracted from each pixel.
         Returns:
             Y: A with A's mean intensity subtracted from each pixel. Output Y is of size 100 x 100.
         """
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+        Y = self.A - np.mean(self.A)
+
         return Y
-    
+
     def prob_2_5(self):
         """
         Create your threshholded A i.e Z here.
         Returns:
             Z: A with only red pixels when the original value of the pixel is above the threshhold. Output Z is of size 100 x 100.
         """
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+        threshold = 0.5
+        # Z = np.where(self.A > threshold, self.A, 0)
+        Z = np.zeros((self.A.shape[0], self.A.shape[1], 3))
+
+        Z[self.A > threshold, 0] = 1  # Set red channel (R) to 1
+
+        plt.imshow(Z)
+        plt.title('2.5 Image')
+        plt.show()
         return Z
-        
-        
-        
+
+
 if __name__ == '__main__':
-    
     p2 = Prob2()
-    
+
     p2.prob_2_1()
     p2.prob_2_2()
-    
+
     X = p2.prob_2_3()
     Y = p2.prob_2_4()
     Z = p2.prob_2_5()
--- hw/ps1/PS1Q3.py
+++ hw/ps1/PS1Q3.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 import cv2
 from skimage import io
 
+
 class Prob3():
     def __init__(self):
         """Load input color image inputPS1Q3.jpg here and assign it as a class variable"""
+        self.img = io.imread("inputPS1Q3.jpg")
         ###### START CODE HERE ######
         ###### END CODE HERE ######
-    
+        pass
+
     def rgb2gray(self, rgb):
         """
         Do RGB to Gray image conversion here. Input is the RGB image and you must return the grayscale image as gray
@@ -16,12 +19,11 @@ class Prob3():
         Returns:
             gray: grayscale image (1 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+
+        gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+
         return gray
-        
+
     def prob_3_1(self):
         """
         Swap red and green color channels here, and return swapImg
@@ -29,12 +31,14 @@ class Prob3():
         Returns:
             swapImg: RGB image with R and G channels swapped (3 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+
+        swapImg = self.img.copy()
+        tmp = swapImg[:, :, 0].copy()
+        swapImg[:, :, 0] = swapImg[:, :, 1]
+        swapImg[:, :, 1] = tmp
+
         return swapImg
-    
+
     def prob_3_2(self):
         """
         This function would simply call your rgb2gray function and return the grayscale image.
@@ -42,13 +46,11 @@ class Prob3():
         Returns:
             grayImg: grayscale image (1 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
 
+        grayImg = self.rgb2gray(self.img)
 
         return grayImg
-    
+
     def prob_3_3(self):
         """
         Convert grayscale image to its negative.
@@ -56,62 +58,94 @@ class Prob3():
         Returns:
             negativeImg: negative image (1 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+
+        negativeImg = 255 - self.prob_3_2()
+        pass
+
         return negativeImg
-    
+
     def prob_3_4(self):
         """
         Create mirror image of gray scale image here.
-        
+
         Returns:
             mirrorImg: mirror image (1 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-    
+
+        mirrorImg = cv2.flip(self.prob_3_2(), 1)
+
         return mirrorImg
-    
+
     def prob_3_5(self):
         """
         Average grayscale image with mirror image here.
-        
+
         Returns:
             avgImg: average of grayscale and mirror image (1 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-        pass
-    
+
+        avgImg = (self.prob_3_2() + self.prob_3_4()) // 2
+
         return avgImg
-    
+
     def prob_3_6(self):
         """
         Create noise matrix N and save as noise.npy. Add N to grayscale image, clip to ensure that max value is 255.
-        
+
         Returns:
             addNoiseImg: grayscale image after adding noise (1 channeled image with integer values lying between 0 - 255)
         """
-        
-        ###### START CODE HERE ######
-        ###### END CODE HERE ######
-        pass
-    
+
+        noise = np.random.randint(0, 255, size=self.prob_3_2().shape, dtype=np.int16)
+        noisyImg = self.prob_3_2() + noise
+        addNoiseImg = np.clip(noisyImg, 0, 255).astype(np.uint8)
+        np.save('noise.npy', noise)
+
         return addNoiseImg
-        
-        
+
+
 if __name__ == '__main__':
-    
     p3 = Prob3()
-    
+
     swapImg = p3.prob_3_1()
     grayImg = p3.prob_3_2()
     negativeImg = p3.prob_3_3()
     mirrorImg = p3.prob_3_4()
     avgImg = p3.prob_3_5()
     addNoiseImg = p3.prob_3_6()
-    
+
+    print(swapImg)
+    print(grayImg)
+    print(negativeImg)
+    print(mirrorImg)
+    print(avgImg)
+    print(addNoiseImg)
+
+    plt.figure(figsize=(12, 12))
+
+    plt.subplot(2, 3, 1)
+    plt.imshow(swapImg)
+    plt.title('Swapped Channels')
+
+    plt.subplot(2, 3, 2)
+    plt.imshow(grayImg, cmap='gray')
+    plt.title('Grayscale Image')
+
+    plt.subplot(2, 3, 3)
+    plt.imshow(negativeImg, cmap='gray')
+    plt.title('Negative Image')
+
+    plt.subplot(2, 3, 4)
+    plt.imshow(mirrorImg, cmap='gray')
+    plt.title('Mirror Image')
+
+    plt.subplot(2, 3, 5)
+    plt.imshow(avgImg, cmap='gray')
+    plt.title('Average of Grayscale and Mirror')
+
+    plt.subplot(2, 3, 6)
+    plt.imshow(addNoiseImg, cmap='gray')
+    plt.title('Image with Noise')
+
+    plt.tight_layout()
+    plt.show()
